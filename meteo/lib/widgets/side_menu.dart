50384a20c46285a8fb0077adcf1c6937dda21fe0
romainsilvy <romain.silvy06@gmail.com>
2022-06-04 10:47:16 +0200

    :tada: add city button working

--- meteo/lib/widgets/side_menu.dart
+++ meteo/lib/widgets/side_menu.dart
@@ -23,19 +23,7 @@ class _NavDrawerState extends State<NavDrawer> {
             if (snapshot.data!.length == 0) {
               return ListView(
                 children: <Widget>[
-                  DrawerHeader(
-                    child: Text(
-                      'Mes villes',
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 25),
-                    ),
-                    decoration: BoxDecoration(
-                        color: Colors.green,
-                        image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: AssetImage('assets/images/city.jpg'))),
-                  ),
+                  _buildDrawerHeader(),
                   ListTile(
                     leading: Icon(Icons.add),
                     title: Text('Ajouter une ville'),
@@ -56,7 +44,7 @@ class _NavDrawerState extends State<NavDrawer> {
             }
             return ListView.builder(
               shrinkWrap: true,
-              itemCount: (snapshot.data!.length ?? 0) + 1,
+              itemCount: (snapshot.data!.length ?? 0) + 2,
               itemBuilder: (BuildContext context, int index) {
                 //print an item on top of the list
                 if (index == 0)
@@ -73,7 +61,20 @@ class _NavDrawerState extends State<NavDrawer> {
                             fit: BoxFit.fill,
                             image: AssetImage('assets/images/city.jpg'))),
                   );
-                else if (index == snapshot.data!.length + 1) {
+                else if (index <= snapshot.data!.length) {
+                  return ListTile(
+                    title: Text(snapshot.data![index - 1].name),
+                    leading: Icon(Icons.location_city),
+                    trailing: IconButton(
+                      icon: new Icon(Icons.delete),
+                      onPressed: () {
+                        handler.deleteCity(snapshot.data![index - 1].name);
+                        setState(() {});
+                      },
+                    ),
+                    onTap: () {},
+                  );
+                } else {
                   return ListTile(
                     leading: Icon(Icons.add),
                     title: Text('Ajouter une ville'),
@@ -88,19 +89,6 @@ class _NavDrawerState extends State<NavDrawer> {
                         setState(() {});
                       }
                     },
-                  );
-                } else {
-                  return ListTile(
-                    title: Text(snapshot.data![index - 1].name),
-                    leading: Icon(Icons.location_city),
-                    trailing: IconButton(
-                      icon: new Icon(Icons.delete),
-                      onPressed: () {
-                        handler.deleteCity(snapshot.data![index - 1].name);
-                        setState(() {});
-                      },
-                    ),
-                    onTap: () {},
                   );
                 }
               },
@@ -137,4 +125,16 @@ class _NavDrawerState extends State<NavDrawer> {
   }
 }
 
+DrawerHeader _buildDrawerHeader() {
+  return DrawerHeader(
+    child: Text(
+      'Mes villes',
+      style: TextStyle(color: Color.fromARGB(255, 255, 255, 255), fontSize: 25),
+    ),
+    decoration: BoxDecoration(
+        color: Colors.green,
+        image: DecorationImage(
+            fit: BoxFit.fill, image: AssetImage('assets/images/city.jpg'))),
+  );
+}
 // class NavDrawer extends StatelessWidget {}
